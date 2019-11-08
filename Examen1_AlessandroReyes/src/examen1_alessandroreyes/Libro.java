427d526d8986eb0d0e4cb1a702779e2c81809905
agreyest <56709605+agreyest@users.noreply.github.com>
2019-11-08 14:02:17 -0600

    clases
    Fin de las clases

--- Examen1_AlessandroReyes/src/examen1_alessandroreyes/Libro.java
+++ Examen1_AlessandroReyes/src/examen1_alessandroreyes/Libro.java
@@ -1,5 +1,108 @@
 package examen1_alessandroreyes;
 
+import java.util.Date;
+
 public class Libro {
+    private String titulo;
+    private String descripcion;
+    private int puntaje;
+    private int copias;
+    private String genero;
+    private int valor;
+    private int edicion;
+    private String autor;
+    private Date publish;
+
+    public Libro() {
+    }
+
+    public Libro(String titulo, String descripcion, int puntaje, int copias, String genero, int valor, int edicion, String autor, Date publish) {
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+        this.puntaje = puntaje;
+        this.copias = copias;
+        this.genero = genero;
+        this.valor = valor;
+        this.edicion = edicion;
+        this.autor = autor;
+        this.publish = publish;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public int getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(int puntaje) {
+        this.puntaje = puntaje;
+    }
+
+    public int getCopias() {
+        return copias;
+    }
+
+    public void setCopias(int copias) {
+        this.copias = copias;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public int getEdicion() {
+        return edicion;
+    }
+
+    public void setEdicion(int edicion) {
+        this.edicion = edicion;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public Date getPublish() {
+        return publish;
+    }
+
+    public void setPublish(Date publish) {
+        this.publish = publish;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
     
 }
--- Examen1_AlessandroReyes/src/examen1_alessandroreyes/Usuario.java
+++ Examen1_AlessandroReyes/src/examen1_alessandroreyes/Usuario.java
@@ -1,5 +1,88 @@
 package examen1_alessandroreyes;
 
+import java.util.ArrayList;
+import java.util.Date;
+
 public class Usuario {
+    private String n_usuario;
+    private String contraseña;
+    private Date naci;
+    private int num_tel;
+    private String email;
+    private String gen_fav;
+    private ArrayList<Libro> lista= new ArrayList();
+
+    public Usuario() {
+    }
+
+    public Usuario(String n_usuario, String contraseña) {
+        this.n_usuario = n_usuario;
+        this.contraseña = contraseña;
+    }
+    
+    public Usuario(String n_usuario, String contraseña, Date naci, int num_tel, String email, String gen_fav) {
+        this.n_usuario = n_usuario;
+        this.contraseña = contraseña;
+        this.naci = naci;
+        this.num_tel = num_tel;
+        this.email = email;
+        this.gen_fav = gen_fav;
+    }
+
+    public String getN_usuario() {
+        return n_usuario;
+    }
+
+    public void setN_usuario(String n_usuario) {
+        this.n_usuario = n_usuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public Date getNaci() {
+        return naci;
+    }
+
+    public void setNaci(Date naci) {
+        this.naci = naci;
+    }
+
+    public int getNum_tel() {
+        return num_tel;
+    }
+
+    public void setNum_tel(int num_tel) {
+        this.num_tel = num_tel;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getGen_fav() {
+        return gen_fav;
+    }
+
+    public void setGen_fav(String gen_fav) {
+        this.gen_fav = gen_fav;
+    }
+
+    public ArrayList<Libro> getLista() {
+        return lista;
+    }
+
+    public void setLista(ArrayList<Libro> lista) {
+        this.lista = lista;
+    }
     
 }
